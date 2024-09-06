3c8193a833c9ca5d8878482cc292b401d6650f8d
Arpita Mondal <job.aarpitaamondal306@gmail.com>
2024-09-07 00:48:18 +0530

    project almost done

--- js/data.js
+++ js/data.js
@@ -3,9 +3,9 @@ let items = [
     id: "1",
     image: "../images/cetaphil.jpg",
     product: "Cetaphil Gentle Skin Cleanser",
-    original_price: "399",
-    current_price: "359",
-    discount: "10",
+    original_price: 399,
+    current_price: 359,
+    discount: 10,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>',
@@ -16,9 +16,9 @@ let items = [
     id: "2",
     image: "../images/dove-bodyscrub.jpg",
     product: "Dove Exfoliating Body Polish - Crushed",
-    original_price: "799",
-    current_price: "599",
-    discount: "25",
+    original_price: 799,
+    current_price: 599,
+    discount: 25,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>',
@@ -29,9 +29,9 @@ let items = [
     id: "3",
     image: "../images/glutahya.jpg",
     product: "Vaseline Gluta-Hya Dewy Radiance & Overnight Radiance Combo",
-    original_price: "685",
-    current_price: "514",
-    discount: "25",
+    original_price: 685,
+    current_price: 514,
+    discount: 25,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>',
@@ -42,9 +42,9 @@ let items = [
     id: "4",
     image: "../images/kay-beauty.jpg",
     product: "Kay Beauty Cream Blush",
-    original_price: "849",
-    current_price: "637",
-    discount: "25",
+    original_price: 849,
+    current_price: 637,
+    discount: 25,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>',
@@ -55,9 +55,9 @@ let items = [
     id: "5",
     image: "../images/lakme-mouse.jpg",
     product: "Lakme Xtraordin-Airy Mattereal Mousse",
-    original_price: "899",
-    current_price: "584",
-    discount: "35",
+    original_price: 899,
+    current_price: 584,
+    discount: 35,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>',
@@ -68,9 +68,9 @@ let items = [
     id: "6",
     image: "../images/loreal-hairserum.jpg",
     product: "L'Oreal Paris Extraordinary Oil Hair Serum, Anti-Frizz",
-    original_price: "649",
-    current_price: "487",
-    discount: "25",
+    original_price: 649,
+    current_price: 487,
+    discount: 25,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>',
@@ -81,9 +81,9 @@ let items = [
     id: "7",
     image: "../images/mac-lipstick.jpg",
     product: "M.A.C Locked Kiss Ink Liquid Lipstick",
-    original_price: "3150",
-    current_price: "3050",
-    discount: "2",
+    original_price: 3150,
+    current_price: 3050,
+    discount: 2,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>',
@@ -94,9 +94,9 @@ let items = [
     id: "8",
     image: "../images/nivea-peral.jpg",
     product: "NIVEA Pearl & Beauty Deodorant Roll On",
-    original_price: "249",
-    current_price: "224",
-    discount: "10",
+    original_price: 249,
+    current_price: 224,
+    discount: 10,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>',
@@ -107,9 +107,9 @@ let items = [
     id: "9",
     image: "../images/sky-high.jpg",
     product: "Maybelline New York Lash Sensational Sky High",
-    original_price: "849",
-    current_price: "509",
-    discount: "40",
+    original_price: 849,
+    current_price: 509,
+    discount: 40,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>',
@@ -120,9 +120,9 @@ let items = [
     id: "10",
     image: "../images/swiss-beauty.jpg",
     product: "Swiss Beauty Long Lasting Professional Makeup Fixer",
-    original_price: "289",
-    current_price: "243",
-    discount: "16",
+    original_price: 289,
+    current_price: 243,
+    discount: 16,
     rating: {
       stars:
         '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i>',
